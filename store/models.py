dae7b058c42b8ced456051815e66ff79597ce222
Yogesh Kumar <yk.301198@gmail.com>
2020-06-07 04:15:56 +0530

    Formatting changes

--- store/models.py
+++ store/models.py
@@ -68,7 +68,8 @@ class ShoppingCartItem(models.Model):
     shopping_cart = models.ForeignKey(ShoppingCart, related_name='items',
                                       related_query_name='item',
                                       on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='+', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='+',
+                                on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     def total(self):
