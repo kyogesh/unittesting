a0c997ba6aad520e2e172596617de900dbd8d9a7
Yogesh Kumar <yk.301198@gmail.com>
2020-06-07 03:46:16 +0530

    Format

--- store/models.py
+++ store/models.py
@@ -65,7 +65,9 @@ class ShoppingCart(models.Model):
 
 
 class ShoppingCartItem(models.Model):
-    shopping_cart = models.ForeignKey(ShoppingCart, related_name='items', related_query_name='item', on_delete=models.CASCADE)
+    shopping_cart = models.ForeignKey(ShoppingCart, related_name='items',
+                                      related_query_name='item',
+                                      on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='+', on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
